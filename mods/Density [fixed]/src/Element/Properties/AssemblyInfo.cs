643320823582bc61e00de13b6a92fed1a474ced7
Nguyễn Cao Hoài Nam <sant1ago.da.hanoi@outlook.com>
2021-07-21 11:21:35 +0700

    add Density source codes

---
+++ mods/Density [fixed]/src/Element/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("2020.1.25.0")]
+[assembly: AssemblyTitle("Element")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Element by sant1ago")]
+[assembly: AssemblyCopyright("Copyright ©  sant1ago 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("02297960-46b3-4380-b917-b65099a2eede")]
+[assembly: AssemblyFileVersion("2020.1.25.0")]
